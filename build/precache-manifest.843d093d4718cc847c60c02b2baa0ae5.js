13ef886839f5785b02bb8fdb89e7e68c5209cbd1
pujarini <pujarini97@gmail.com>
2020-10-22 19:48:19 +0530

    First commit

---
+++ build/precache-manifest.843d093d4718cc847c60c02b2baa0ae5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a2c3f9409b01fb50cb3d12eb631565",
+    "url": "/brain-app/index.html"
+  },
+  {
+    "revision": "49df4a5ae3b048294245",
+    "url": "/brain-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5ef07f2d3f3eb54a5094",
+    "url": "/brain-app/static/css/main.a10ddf20.chunk.css"
+  },
+  {
+    "revision": "49df4a5ae3b048294245",
+    "url": "/brain-app/static/js/2.d637a052.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/brain-app/static/js/2.d637a052.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ef07f2d3f3eb54a5094",
+    "url": "/brain-app/static/js/main.b1c7ff17.chunk.js"
+  },
+  {
+    "revision": "fe9cdd326b364bf7c2d2",
+    "url": "/brain-app/static/js/runtime-main.1cf9eeb1.js"
+  },
+  {
+    "revision": "3d276786db39cd57a6dbcb701379a570",
+    "url": "/brain-app/static/media/brain.3d276786.png"
+  }
+]);
